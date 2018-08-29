512a429ee9f03447bf1b721309a6342be9aa334b
zbotot <42085123+zbotot@users.noreply.github.com>
2018-08-29 12:07:06 +0700

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -19,7 +19,7 @@ class Config(object):
     LINE_SQUARE_QUERY_PATH      = '/SQS1'
 
     CHANNEL_ID = {
-        'LINE_TIMELINE': '1341209950',
+        'LINE_TIMELINE': '1341209850',
         'LINE_WEBTOON': '1401600689',
         'LINE_TODAY': '1518712866',
         'LINE_STORE': '1376922440',
@@ -28,10 +28,10 @@ class Config(object):
     }
 
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[96]
-    APP_VER     = '8.0.2'
+    APP_VER     = '2.1.5'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'Nadya'
-    SYSTEM_VER  = '11.2.5'
+    SYSTEM_NAME = 'sepriche'
+    SYSTEM_VER  = '10.12.0'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
